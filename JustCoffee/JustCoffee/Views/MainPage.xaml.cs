dd5992967d76ee5d8451d8b14be702f82484ca64
vsov <vsov@CARDS>
2017-02-07 10:02:14 +0200

    Add project files.

---
+++ JustCoffee/JustCoffee/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace JustCoffee.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
